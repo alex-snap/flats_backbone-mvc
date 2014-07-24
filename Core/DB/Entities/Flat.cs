2bc632f69595b7e9a12349db190cd254d08cfe30
ayrat404@gmail.com <ayrat404@gmail.com>
2014-07-24 23:30:35 +0400

    db

---
+++ Core/DB/Entities/Flat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DB.Entities
+{
+    public class Flat: Entity
+    {
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Roooms { get; set; }
+        public string MainImg { get; set; }
+        public string Address { get; set; }
+
+        public virtual ICollection<Image> Images { get; set; }
+        public virtual ICollection<FlatBusyDate> FlatBusyDates { get; set; }
+    }
+}
