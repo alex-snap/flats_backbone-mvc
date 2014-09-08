ef24e7fd0ba677b944bbe4ba4ef7556041deca36
ayrat404@gmail.com <ayrat404@gmail.com>
2014-09-08 20:48:10 +0400

    +

---
+++ DAL/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace DAL.Entities
+{
+    public class User: IdentityUser
+    {
+
+    }
+}
---
+++ DAL/UserManager.cs
@@ -0,0 +1,26 @@
+﻿using DAL.Entities;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+
+namespace DAL
+{
+    public class FlatsUserManager: UserManager<User>
+    {
+        public FlatsUserManager(IUserStore<User> store): base(store)
+        {
+            
+        }
+
+        public static FlatsUserManager Create(
+            IdentityFactoryOptions<FlatsUserManager> options,
+            IOwinContext context)
+        {
+            FlatsIdentityDbContext db = context.Get<FlatsIdentityDbContext>();
+            FlatsUserManager manager = new FlatsUserManager(new UserStore<User>(db));
+            return manager; 
+        }
+
+    }
+}
