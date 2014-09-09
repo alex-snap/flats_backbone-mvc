75bc0b20338492b7f2477db5db09edc746318011
ayrat404@gmail.com <ayrat404@gmail.com>
2014-09-09 21:28:56 +0400

    +

---
+++ DAL/FlatsIdentityDbContext.cs
@@ -0,0 +1,35 @@
+﻿using System.Data.Entity;
+using DAL.Entities;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace DAL
+{
+    public class FlatsIdentityDbContext: IdentityDbContext<User>
+    {
+        public FlatsIdentityDbContext() : base("EFDbContext") { }
+
+        static FlatsIdentityDbContext()
+        {
+            Database.SetInitializer<FlatsIdentityDbContext>(new IdentityDbInit());
+        }
+
+        public static FlatsIdentityDbContext Create()
+        {
+            return new FlatsIdentityDbContext();
+        }
+    }
+
+    public class IdentityDbInit : DropCreateDatabaseIfModelChanges<FlatsIdentityDbContext>
+    {
+        protected override void Seed(FlatsIdentityDbContext context)
+        {
+            PerformInitialSetup(context);
+            base.Seed(context);
+        }
+
+        public void PerformInitialSetup(FlatsIdentityDbContext context)
+        {
+            //
+        }
+    }
+}
---
+++ flats/App_Start/IdentityConfig.cs
@@ -0,0 +1,23 @@
+﻿using DAL;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+namespace flats
+{
+    public class IdentityConfig
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.CreatePerOwinContext<FlatsIdentityDbContext>(FlatsIdentityDbContext.Create);
+            app.CreatePerOwinContext<FlatsUserManager>(FlatsUserManager.Create);
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                //LoginPath = new PathString()
+            });
+        }
+    }
+}
