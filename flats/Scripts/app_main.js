228900ad6ca1198bcae8986c1bff7ab5dbae3caa
aryabov-igs <snap.alex@mail.ru>
2014-09-16 10:12:28 +0400

    +

--- flats/Scripts/app_main.js
+++ flats/Scripts/app_main.js
@@ -5,50 +5,47 @@ function (Marionette) {
 
     // Создаем экземпляр главного приложения
     // ---------------------
-    var FlatsManager = new Marionette.Application();
+    var ModulBank = new Marionette.Application();
 
     // Описываем regions главного приложения
     // ---------------------
-    FlatsManager.addRegions({
+    ModulBank.addRegions({
         headerRegion    :   '#header-region',
         mainRegion      :   '#main-region',
         footerRegion    :   '#footer-region'
-        //dialogRegion: Marionette.Region.Dialog.extend({
-        //    el: '#dialog-region'
-        //})
     });
 
     // Метод изменения текущего роута
     // ---------------------
-    FlatsManager.navigate = function(route, options) {
+    ModulBank.navigate = function (route, options) {
         options || (options = {});
         Backbone.history.navigate(route, options);
     };
 
     // Метод получения текущего роута
     // ---------------------
-    FlatsManager.getCurrentRoute = function() {
+    ModulBank.getCurrentRoute = function () {
         return Backbone.history.fragment;
     };
 
     // Метод получения роута без аргументов
     // ---------------------
-    FlatsManager.getBaseRoute = function () {
+    ModulBank.getBaseRoute = function () {
         var r = Backbone.history.fragment.substring(0, Backbone.history.fragment.indexOf('?'));
-        return r.length ? r : FlatsManager.getCurrentRoute();
+        return r.length ? r : ModulBank.getCurrentRoute();
     };
 
     // Метод для управления остановкой/запуском модулей
     // ---------------------
-    FlatsManager.startSubApp = function (appName, args) {
-        var startedApp = appName ? FlatsManager.module(appName) : null;
-        if (FlatsManager.currentApp === startedApp) { return; }
+    ModulBank.startSubApp = function (appName, args) {
+        var startedApp = appName ? ModulBank.module(appName) : null;
+        if (ModulBank.currentApp === startedApp) { return; }
 
-        if (FlatsManager.currentApp) {
-            FlatsManager.currentApp.stop();
+        if (ModulBank.currentApp) {
+            ModulBank.currentApp.stop();
         }
 
-        FlatsManager.currentApp = startedApp;
+        ModulBank.currentApp = startedApp;
         if (startedApp) {
             startedApp.start(args);
         }
@@ -58,41 +55,18 @@ function (Marionette) {
     // для инициализации их роутов до Backbone.history.start() и 
     // запускаем подприложение flats с методом list
     // ---------------------
-    FlatsManager.on('start', function () {
+    ModulBank.on('start', function () {
         if (Backbone.history) {
             require(['app/flats', 'app/test'], function () {
                 Backbone.history.start();
-                if (FlatsManager.getCurrentRoute() === '') {
-                    FlatsManager.trigger('flats:list');
+                if (ModulBank.getCurrentRoute() === '') {
+                    ModulBank.trigger('flats:list');
                 }
             });
         }
-
-        // инициализация модуля Common.Views
-        // и отрисовка общего футера
-        // ---------------
-        require(['commonViews'], function () {
-            var footerView = new FlatsManager.Common.Views.Footer();
-            FlatsManager.footerRegion.show(footerView);
-        });
-        console.log('FlatsManager: started');
+        console.log('ModulBank: started');
     });
 
-    // backbone.history - это глобальный роутер для всего приложения
-    // поэтому мы можем отлавливая событие 'route' узнавать о смене url
     // ---------------------
-    Backbone.history.on('route', function (controller, method) {
-        scrollUp();
-    });
-
-    // функция прокрутки страницы вверх
-    // ---------------------
-    function scrollUp() {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 150);
-    }
-
-    // ---------------------
-    return FlatsManager;
+    return ModulBank;
 });
